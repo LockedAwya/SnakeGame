a3ce1959655fc0520ac27f0db9daa35ec1d8d078
LockedAwya <58223975+LockedAwya@users.noreply.github.com>
2020-05-11 14:54:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,136 +1,149 @@
 #include <SDL.h>
 #include <cstdlib>
 #include <iostream>
+#include <SDL_image.h>
+#include "Snake.h"
+#include "Food.h"
+#include "Obstacles.h"
+#include "buttons.h"
+#include "Pictures.h"
+//#include "CommonFunction.h"
 
-using namespace std;
+const int HEIGHT = 600;
+const int WIDTH = 600;
+bool IsPlaying = true;
+bool Isrunning = true;
+bool IsInstruction = true;
+bool TurnOffSound = true;
+bool TurnOnSound = true;
 
-const int HEIGHT = 800;
-const int WIDTH = 800;
-const int tail_max = 1000;
-int score;
+SDL_Event g_event; //gameplay event
+SDL_Event m_event; //menu event
+SDL_Event i_event; //instruction event
 
-struct Food {
-    int FoodX, FoodY; //coordinate of food
-    int size = 10;
-    void generateFood() {
-        FoodX = rand() % (WIDTH / size);
-        FoodY = rand() % (HEIGHT / size);
+SDL_Window *window;
+
+void Menu(SDL_Renderer *renderer, Pictures &picture, Button &button)
+{
+    picture.getMenuBackground(renderer);
+    button.getButtonInstruction(renderer);
+    button.getButtonPlay(renderer);
+    button.getButtonExit(renderer);
+    button.getButtonSoundOn(renderer);
+    //button.getButtonSoundOff(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Instruction(SDL_Renderer *renderer, SDL_Event i_event, Pictures &picture, Button &button)
+{
+    ButtonCoor buttoncoor = {};
+    picture.getInstructionBackground(renderer);
+    button.getButtonBack(renderer);
+    SDL_RenderPresent(renderer);
+    while(SDL_PollEvent(&i_event)) {
+    if (i_event.type == SDL_QUIT) {
+        IsInstruction = false;
+        //Isrunning = false;
+        }
+    if (i_event.type == SDL_KEYDOWN) {
+        switch (i_event.key.keysym.sym) {
+        case SDLK_b:
+            IsInstruction = false;
+        }
     }
-
-    void draw(SDL_Renderer* renderer) { //draw food
-        SDL_Rect rect;
-        rect.w = size;
-        rect.h = size;
-        rect.x = size * FoodX;
-        rect.y = size * FoodY;
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
     }
+}
 
-};
-
-struct PosAndVel {
-    int x, y; //position
-    int stepX = 0, stepY = 0; //velocity
-};
-
-struct Snake {
-    PosAndVel position; //coordinate of the snake
-    PosAndVel velocity; //speed of the snake (which has directions)
-    int size = 10;
-    int tail_start, tail_end; //head, //tail
-    PosAndVel tail[tail_max];
-    int tail_length = 1000;
-    void move() {
-        position.x += velocity.stepX;
-        position.y += velocity.stepY;
-    }
-    void update(Food& food) {
-            tail_start++;
-            tail_end++;
-            tail[tail_end % tail_max] = position;
-            position.x += velocity.x;
-            position.y += velocity.y;
-            if (position.x == food.FoodX && position.y == food.FoodY) {
-                tail_length++;
-                tail_start--;
-                food.generateFood();
-                score++;
-                cout << score << ":" << " " << endl;
-            }
-            for (int i = 0; i < tail_length; i++) {
-                PosAndVel& tail_position = tail[(tail_start + i) % tail_max];
-                if (tail_position.x == position.x && tail_position.y == position.y) {
-                    //lose
-                    tail_length = 0;
-                    tail_start = tail_end;
-                }
+void GamePlay(SDL_Renderer *renderer, SDL_Event g_event, Pictures &picture, Obstacles &obstacles, Food &food, Snake &snake, Pos& tail_position)
+{
+    snake.move();
+    picture.getMenuBackground(renderer);
+    snake.update(food, obstacles, picture, renderer);
+    obstacles.draw(renderer);
+    snake.draw(renderer, snake, tail_position);
+    food.draw(renderer);
+    SDL_RenderPresent(renderer);
+    while (SDL_PollEvent(&g_event)) {
+        if (g_event.type == SDL_QUIT) {
+            IsPlaying = false;
+            Isrunning = false;
+        }
+        if (g_event.type == SDL_KEYDOWN) {
+            switch (g_event.key.keysym.sym) {
+            case SDLK_ESCAPE:
+                IsPlaying = false;
+                break;
+            case SDLK_UP:
+                snake.TurnUp();
+                break;
+            case SDLK_DOWN:
+                snake.TurnDown();
+                break;
+            case SDLK_LEFT:
+                snake.TurnLeft();
+                break;
+            case SDLK_RIGHT:
+                snake.TurnRight();
+                break;
+            default:
+                break;
             }
         }
-    void draw(SDL_Renderer* renderer, Food& food) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        for (int i = 0; i < tail_length; i++) {
-            PosAndVel& tail_position = tail[(tail_start + i) % tail_max];
-            SDL_Rect rect{size * tail_position.x, size * tail_position.y, size, size};
-            SDL_RenderFillRect(renderer, &rect);
-        }
-        SDL_Rect rect{size * position.x, size * position.y, size, size};
-        SDL_RenderFillRect(renderer, &rect);
     }
-};
+}
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("Snake Game!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Event e;
-    bool Isrunning = true;
+    Pictures picture = {};
+    Button button = {};
+    ButtonCoor buttoncoor = {};
     Snake snake = {};
     Food food = {};
-    food.generateFood();
+    Obstacles obstacles = {};
+    Pos tail_position = {};
+    window = SDL_CreateWindow("Snake Game!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     while(Isrunning) {
-        snake.move();
-        SDL_Delay(75);
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+        Menu(renderer, picture, button);
+        while (SDL_PollEvent(&m_event)) {
+            if (m_event.type == SDL_QUIT) { //e
                 Isrunning = false;
             }
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
-                case SDLK_ESCAPE:
-                    Isrunning = false;
-                    break;
-                case SDLK_UP:
-                    snake.velocity.y = -1;
-                    snake.velocity.x = 0;
-                    break;
-                case SDLK_DOWN:
-                    snake.velocity.y = 1;
-                    snake.velocity.x = 0;
-                    break;
-                case SDLK_LEFT:
-                    snake.velocity.y = 0;
-                    snake.velocity.x = -1;
-                    break;
-                case SDLK_RIGHT:
-                    snake.velocity.y = 0;
-                    snake.velocity.x = 1;
-                    break;
-                default:
-                    break;
+            if (m_event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                int x = m_event.motion.x;
+                int y = m_event.motion.y;
+                if (x >= buttoncoor.bPX && x <= buttoncoor.bPX + buttoncoor.bPW && y >= buttoncoor.bPY && y <= buttoncoor.bPY + buttoncoor.bPH) //click PlayGame
+                {//SDL_ShowSimpleMessageBox(0, "Mouse", "PlayButton clicked!", window);
+                     IsPlaying = true;
+                     while(IsPlaying) {
+                        GamePlay(renderer, g_event, picture, obstacles, food, snake, tail_position);
+                    }
+                }
+            else if (x >= buttoncoor.bIX && x <= buttoncoor.bIX + buttoncoor.bIW && y >= buttoncoor.bIY && y <= buttoncoor.bIY + buttoncoor.bIH) //click instruction
+            {   //SDL_ShowSimpleMessageBox(0, "Mouse", "InstructionButton clicked!", window);
+                IsInstruction = true;
+                while (IsInstruction) {
+                    Instruction(renderer, i_event, picture, button);
                 }
             }
+            else if (x >= buttoncoor.bEX && x <= buttoncoor.bEX + buttoncoor.bEW && y >= buttoncoor.bEY && y <= buttoncoor.bEY + buttoncoor.bEH) //click exit
+            {
+                Isrunning = false;
+            }
+            }
         }
-        snake.update(food);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        snake.draw(renderer, food);
-        food.draw(renderer);
-
-        SDL_RenderPresent(renderer);
     }
+        //SDL_RenderPresent(renderer);
+    //Destroy a window.
+	SDL_DestroyWindow(window);
 
-    return 0;
+	//Destroy a renderer
+	SDL_DestroyRenderer(renderer);
+
+	//cleans up all initialized subsystems
+	SDL_Quit();
+	return 0;
 }
+
+
